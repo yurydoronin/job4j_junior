34e8f54b4d5dc1312a3c5e77380bb523c1caa0f2
Yury Doronin <doronin.ltd@gmail.com>
2020-04-14 18:33:44 +0300

    code refactoring

---
+++ chapter_007/src/main/java/ru/job4j/logger/UsageLog4j.java
@@ -0,0 +1,17 @@
+package ru.job4j.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsageLog4j {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class);
+
+    public static void main(String[] args) {
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
+    }
+}
